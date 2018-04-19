e25a9bd731d6dfbb0e2c09d2f08eb8e3dad6bdfd
Robert <user@USER-KOMPUTER>
2018-04-19 23:01:06 +0200

    Good working program

---
+++ EMedia/RSA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace EMedia
+{
+    class RSA
+    {
+        public static long square(long s)
+        {
+            return (s * s);
+        }
+
+        public static long Mod(int e, int p, int q)
+        {
+            if(p==0)
+            {
+                return 1;
+            }
+            else if(p%2==0)
+            {
+                return square(Mod(e, p / 2, q)) % q;
+            }
+            else
+                return ((e%q) * Mod(e, p-1,q)) % q;
+        }
+    }
+}
